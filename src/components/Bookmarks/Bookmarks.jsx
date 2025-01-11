692c407a956ab2fa1d5adf85635e99be0b403667
WskyMike <originid1488@ya.ru>
2025-01-11 14:46:53 +0300

    dev: add filtering by type on bookmarks

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, NavDropdown } from "react-bootstrap";
 import MovieCard from "../MovieCarousel/MovieCard/Moviecard";
 import SearchForm from "../SearchForm/SearchForm";
 import ScrollToTopButton from "../../vendor/ScrollToTopButton/ToTopButton";
@@ -10,54 +10,64 @@ import BackwardButton from "../../vendor/BackwardButton/BackwardButton";
 import { useAuth } from "../../contexts/AuthContext";
 import { useLoading } from "../../contexts/LoadingContext";
 import { MovieCardByIdData } from "../../utils/MovieCardByIdApi";
-import { TvCardByIdData } from "../../utils/TvCardByIdApi"
+import { TvCardByIdData } from "../../utils/TvCardByIdApi";
 
 import { db } from "../../utils/firebase";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, where } from "firebase/firestore";
 
 function Bookmarks() {
   const { user } = useAuth();
   const [bookmarkedMovies, setBookmarkedMovies] = useState([]);
   const { bookmarksLoading, setBookmarksLoading } = useLoading();
+  const [filterType, setFilterType] = useState("all");
 
-  // Запрос закладок из БД и фетчинг данных фильмов
-  useEffect(() => {
-    if (user) {
-      const fetchBookmarks = async () => {
-        setBookmarksLoading(true);
-        try {
-          const bookmarksQuery = query(
-            collection(db, "users", user.uid, "bookmarks"),
-            orderBy("timestamp", "asc")
-          );
-          const querySnapshot = await getDocs(bookmarksQuery);
+  // Запрос данных из Firestore
+  const fetchBookmarks = async (type) => {
+    if (!user) return;
 
-          // Формируем запросы для фильмов и сериалов
-          const bookmarks = querySnapshot.docs.map((doc) => ({
-            id: doc.data().itemId,
-            mediaType: doc.data().mediaType,
-          }));
+    setBookmarksLoading(true);
+    try {
+      // Создаём запрос к Firestore
+      let bookmarksQuery = query(
+        collection(db, "users", user.uid, "bookmarks"),
+        orderBy("timestamp", "asc")
+      );
 
-          const items = await Promise.all(
-            bookmarks.map(
-              (bookmark) =>
-                bookmark.mediaType === "movie"
-                  ? MovieCardByIdData(bookmark.id) // Запрос к API фильмов
-                  : TvCardByIdData(bookmark.id) // Запрос к API сериалов
-            )
-          );
+      if (type !== "all") {
+        bookmarksQuery = query(
+          collection(db, "users", user.uid, "bookmarks"),
+          where("mediaType", "==", type),
+          orderBy("timestamp", "asc")
+        );
+      }
 
-          setBookmarkedMovies(items);
-        } catch (error) {
-          console.error("Error fetching bookmarks:", error);
-        } finally {
-          setBookmarksLoading(false);
-        }
-      };
+      const querySnapshot = await getDocs(bookmarksQuery);
+      const bookmarks = querySnapshot.docs.map((doc) => ({
+        id: doc.data().itemId,
+        mediaType: doc.data().mediaType,
+      }));
 
-      fetchBookmarks();
+      // Получаем данные для каждого фильма или сериала
+      const items = await Promise.all(
+        bookmarks.map((bookmark) =>
+          bookmark.mediaType === "movie"
+            ? MovieCardByIdData(bookmark.id)
+            : TvCardByIdData(bookmark.id)
+        )
+      );
+
+      setBookmarkedMovies(items);
+    } catch (error) {
+      console.error("Ошибка при загрузке закладок:", error);
+    } finally {
+      setBookmarksLoading(false);
     }
-  }, [user, setBookmarksLoading]);
+  };
+
+  // Обновляем закладки при изменении фильтра
+  useEffect(() => {
+    fetchBookmarks(filterType);
+  }, [filterType]);
 
   return (
     <>
@@ -67,8 +77,37 @@ function Bookmarks() {
         <BackwardButton />
         <SearchForm />
         <Container fluid="xl">
-          <Row className="pt-2 sticky-header">
-            <h2 className="text-start display-5">Буду смотреть</h2>
+          <Row className="pt-2 sticky-header d-flex justify-content-between align-items-center">
+            <Col xs="auto">
+              <h2 className="text-start display-5">Буду смотреть</h2>
+            </Col>
+            {!bookmarksLoading && bookmarkedMovies.length > 0 && (
+              <Col xs="auto" className="ms-md-3">
+                <NavDropdown
+                  id="filter-dropdown"
+                  title={
+                    filterType === "all"
+                      ? "Тип контента "
+                      : filterType === "movie"
+                      ? "Фильмы "
+                      : "Сериалы "
+                  }
+                  variant="light"
+                  className="movie-list__dropdown-button"
+                  onSelect={(selectedKey) => setFilterType(selectedKey)}
+                >
+                  <div className="movie-list__custom-scroll">
+                    <NavDropdown.Item eventKey="all">Все</NavDropdown.Item>
+                    <NavDropdown.Item eventKey="movie" className="fw-light">
+                      Фильмы
+                    </NavDropdown.Item>
+                    <NavDropdown.Item eventKey="tv" className="fw-light">
+                      Сериалы
+                    </NavDropdown.Item>
+                  </div>
+                </NavDropdown>
+              </Col>
+            )}
           </Row>
           <Row className="mb-2 mb-md-5 mt-4">
             {bookmarksLoading ? (
@@ -77,7 +116,7 @@ function Bookmarks() {
                   <span className="visually-hidden">Загрузка...</span>
                 </div>
               </Col>
-            ) : (
+            ) : bookmarkedMovies.length > 0 ? (
               bookmarkedMovies.map((movie) => (
                 <Col
                   xs={6}
@@ -90,6 +129,30 @@ function Bookmarks() {
                   <MovieCard movie={movie} isLoading={false} />
                 </Col>
               ))
+            ) : (
+              <Col
+                className="text-center"
+                style={{ fontFamily: "Inter, sans-serif" }}
+              >
+                <p className="mt-5 fs-5 fw-bold">У вас ещё нет закладок.</p>
+                <p className="text-muted mt-5 fs-6">
+                  Добавьте{" "}
+                  <a
+                    href="http://localhost:5173/list/popular"
+                    className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                  >
+                    фильмы
+                  </a>{" "}
+                  или{" "}
+                  <a
+                    href="http://localhost:5173/list/popularTv"
+                    className="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+                  >
+                    сериалы
+                  </a>
+                  , которые хотите посмотреть.
+                </p>
+              </Col>
             )}
           </Row>
         </Container>
